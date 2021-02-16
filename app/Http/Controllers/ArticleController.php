5c17ba6e2e951068249daeb9a06cb13aa427ae20
kei-1011 <k_kojima0407@yahoo.co.jp>
2021-02-16 18:42:51 +0900

    記事更新処理でタグを登録可能にする

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -54,7 +54,14 @@ class ArticleController extends Controller {
      */
     public function edit(Article $article)
     {
-        return view('articles.edit',['article' => $article]);
+        $tagNames = $article->tags->map(function ($tag) {
+            return ['text' => $tag->name];
+        });
+
+        return view('articles.edit', [
+            'article' => $article,
+            'tagNames' => $tagNames,
+        ]);
     }
 
     /**
@@ -63,6 +70,13 @@ class ArticleController extends Controller {
     public function update(ArticleRequest $request,Article $article)
     {
         $article->fill($request->all())->save();
+
+        // 更新対象の記事とタグの紐付けをいったん全削除
+        $article->tags()->detach();
+        $request->tags->each(function ($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
         return redirect()->route('articles.index');
     }
 
