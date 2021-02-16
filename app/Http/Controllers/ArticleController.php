3a4d9fb7fcc57189b29f13056b3b5aa004461be7
kei-1011 <k_kojima0407@yahoo.co.jp>
2021-02-16 17:42:29 +0900

    記事投稿処理とタグモデルの編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Tag;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,9 @@ class ArticleController extends Controller {
         $this->authorizeResource(Article::class, 'article');
     }
 
+    /**
+     * 記事一覧画面
+     */
     public function index() {
         $articles = Article::all()->sortByDesc('created_at');
 
@@ -21,40 +25,67 @@ class ArticleController extends Controller {
         // return view('articles.index', ['articles' => $articles]);
     }
 
+    /**
+     * 新規投稿画面
+     */
     public function create() {
         return view('articles.create');
     }
 
+    /**
+     * 新規投稿アクション
+     */
     public function store(ArticleRequest $request, Article $article)
     {
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
+
+        //コレクションの各要素に対して順に処理
+        $request->tags->each(function($tagName) use ($article) {
+            $tag = Tag::firstOrCreate(['name' => $tagName]);
+            $article->tags()->attach($tag);
+        });
         return redirect()->route('articles.index');
     }
 
+    /**
+     * 記事更新画面
+     */
     public function edit(Article $article)
     {
         return view('articles.edit',['article' => $article]);
     }
 
+    /**
+     * 記事更新アクション
+     */
     public function update(ArticleRequest $request,Article $article)
     {
         $article->fill($request->all())->save();
         return redirect()->route('articles.index');
     }
 
+    /**
+     * 記事削除アクション
+     */
     public function destroy(Article $article)
     {
         $article->delete();
         return redirect()->route('articles.index');
     }
 
+    /**
+     * 記事詳細画面
+     */
     public function show(Article $article)
     {
         return view('articles.show', ['article' => $article]);
     }
 
+    /**
+     * いいねアクション
+     */
     public function like(Request $request, Article $article)
     {
         $article->likes()->detach($request->user()->id);
@@ -66,6 +97,9 @@ class ArticleController extends Controller {
         ];
     }
 
+    /**
+     * いいね解除アクション
+     */
     public function unlike(Request $request, Article $article)
     {
         $article->likes()->detach($request->user()->id);
